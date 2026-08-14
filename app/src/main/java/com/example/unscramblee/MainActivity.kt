03c2564ca9610188b15d996a4605eeea779eeb2c
haidiefajardo13-ui <haidiefajardo13-ui>
2026-08-14 10:52:36 +0800

    Phase 1

---
+++ app/src/main/java/com/example/unscramblee/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.unscramblee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.unscramblee.ui.theme.UnscrambleeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            UnscrambleeTheme {
+                GameScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun GameScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "UNSCRAMBLE",
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = "TAC",
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "Unscramble the word!"
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = {
+                Text("Enter your answer")
+            }
+        )
+
+        Button(
+            onClick = { }
+        ) {
+            Text("SUBMIT")
+        }
+
+        Text(
+            text = "Score: 0"
+        )
+    }
+}
